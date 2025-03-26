6f274fff27b49fb1f7013daa7fc17b3d32ffdbb5
Your Name <you@example.com>
2025-03-26 08:58:08 -0300

    new try

--- list.c
+++ list.c
@@ -40,12 +40,14 @@ void * firstList(List * list) {
     if(list->head == NULL)
         return NULL;
     
-    list->current = list->head;
-    return list->current->data;
+    return list->head->data;
 }
 
 void * nextList(List * list) {
-    
+    if(list->current != NULL)
+        list->current = list->current->next; //head, current, y tail es de lista
+    else                                        //el nodo es el que tiene prev, next y data; 
+        return NULL;
 }
 
 void * lastList(List * list) {
